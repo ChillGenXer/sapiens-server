4a3c0a6e5d68d8abc979a22da6aa85d57e95bb2e
ChillGenXer <81128365+ChillGenXer@users.noreply.github.com>
2024-04-26 10:06:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,3 +9,18 @@ steamcmd +runscript ~/sapiens-server/steamupdate.txt
 mkdir ~/.steam/sdk64
 cd ~/.steam/sdk64/
 ln -s ../steam/steamcmd/linux64/steamclient.so steamclient.so
+# Define the directory to add to the PATH
+DIR="$HOME/sapiens-server"
+
+# Check if the directory is already in the PATH
+if [[ ":$PATH:" != *":$DIR:"* ]]; then
+    # Add the directory to the PATH in .bashrc if it's not already included
+    echo "export PATH=\"\$PATH:$DIR\"" >> ~/.bashrc
+    
+    # Source the .bashrc to update the current session
+    source ~/.bashrc
+
+    echo "Directory $DIR added to PATH."
+else
+    echo "Directory $DIR is already in PATH."
+fi
