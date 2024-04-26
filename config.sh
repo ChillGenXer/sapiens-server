deb3a0307cb76aa2203c5ac0ee35a7ae55f74d04
ChillGenXer <chillgenxer@gmail.com>
2024-04-25 23:56:42 -0400

    Updated some comments.

--- config.sh
+++ config.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-#Location of where the server software is located.  If you followed the tutorial you probably just need to change YOUR_USERNAME.  Don't use
+# Location of where the server software is located.  If you followed the tutorial you probably just need to change YOUR_USERNAME.  Don't use
 # "~" to shorten the home directory, I had some problems with it not being interpreted correctly in the script.
 GAME_DIR="/home/YOUR_USERNAME/.local/share/Steam/steamcmd/sapiens"
 
-#Location of where the world is.  You will need to see what was generated and set it here.
+# Location of where the world is.  You will need to see what was generated and set it here.
 WORLD_DIR="/home/YOUR_USERNAME/.local/share/majicjungle/sapiens/players/PLAYER_NUMBER_FOLDER_NAME/worlds/WORLD_NUMBER_FOLDER_NAME"
 
-#The folder where the backup command will send your world archive.  This folder was created with the git package, just change as necessary. 
+# The folder where the backup command will send your world archive.  This folder was created with the git package, just change as necessary. 
 BACKUP_DIR="/home/YOUR_USERNAME/sapiens-server/world_backups"
 
-#World Name.  This is the name used in the game that shows up on the map as well as the server listing.
+# World Name.  This is the name used in the game that shows up on the map as well as the server listing.
 WORLD_NAME="Name of the World"
 
-#Network settings.  These are the defaults, change as necessary.
+# Network settings.  These are the defaults, change as necessary.  Please note there is 1 additional port that needs to be opened on your
+# firewall that will be +1 above the UDP_PORT.  So using the defaults you would have to open 16161 and 16162 for UDP and 16168 for TCP 
+# redirected to your server
 UDP_PORT=16161
 HTTP_PORT=16168
 
-#Best to leave this as is.
+# Best to leave this as is.
 SCREEN_NAME="sapiens-server"
--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Script for starting the server.  Its purpose is to start the server and automatically
-# restart the server if it returns a non-zero argument on exit, implying a crash.
+# restart it if a non-zero argument is returned on exit, implying a crash.  This approach isn't 
+# bulletproof but it works pretty good.
 
-#Import our configuration
+# Import your configuration
 source config.sh
 
 while true; do
     # Start the server with the world name passed when starting the script.  If you need to modify
-    #any of the server arguments you can do it here.
+    # any of the server arguments you can do it here.
     cd $GAME_DIR
     ./linuxServer --load "$WORLD_NAME" --port "$UDP_PORT" --http-port "$HTTP_PORT"
 
@@ -23,5 +24,5 @@ while true; do
 
     # Add a delay before restarting the server
     sleep 5
-	echo "Restarting server..."
+	echo "Restarting Sapiens server..."
 done
