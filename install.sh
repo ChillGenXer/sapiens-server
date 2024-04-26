d0c12254c36dc49c031c7fdc7edbfd61913e7d3f
ChillGenXer <chillgenxer@gmail.com>
2024-04-26 00:40:28 -0400

    Added sapiens server installation

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ sudo add-apt-repository multiverse
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install steamcmd
+steamcmd +runscript ~/sapiens-server/steamupdate.txt
+mkdir ~/.steam/sdk64
+cd ~/.steam/sdk64/
+ln -s ../steam/steamcmd/linux64/steamclient.so steamclient.so
