6f99f3cf7a98aa112085bd3e3149c3e41b7df4e5
ChillGenXer <chillgenxer@gmail.com>
2024-04-26 01:45:07 -0400

    Updated example config to match readme.

--- config.sh
+++ config.sh
@@ -11,7 +11,7 @@ WORLD_DIR="/home/YOUR_USERNAME/.local/share/majicjungle/sapiens/players/PLAYER_N
 BACKUP_DIR="/home/YOUR_USERNAME/sapiens-server/world_backups"
 
 # World Name.  This is the name used in the game that shows up on the map as well as the server listing.
-WORLD_NAME="Name of the World"
+WORLD_NAME="My New World"
 
 # Network settings.  These are the defaults, change as necessary.  Please note there is 1 additional port that needs to be opened on your
 # firewall that will be +1 above the UDP_PORT.  So using the defaults you would have to open 16161 and 16162 for UDP and 16168 for TCP 
