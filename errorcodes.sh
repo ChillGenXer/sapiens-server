7c02ee6773fa742c03e8fe1d4f2737cb6c60b3d1
chillgenxer <chillgenxer@gmail.com>
2024-05-09 04:30:28 +0000

    Just added a script library check to errorcodes.sh for later.

--- errorcodes.sh
+++ errorcodes.sh
@@ -2,6 +2,12 @@
 # Author: ChillGenXer (chillgenxer@gmail.com)
 # Description: Error code array to leverage bash script exit codes for better error handling.
 
+# Check if the script is being run directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    echo "This is a library script file, and not meant to be run directly. Run sapiens.sh only."
+    exit 1
+fi
+
 declare -A ERRORCODE=(
     [0]="Operation completed successfully."
     [1]="Error 1"
@@ -10,4 +16,3 @@ declare -A ERRORCODE=(
     [4]="Error 4"
     [5]="Error 5"
 )
-
